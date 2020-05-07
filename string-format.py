a602c45e9777b7d6bd1ee8e523da429545f76bf5
Catherine <catherineep4189@gmail.com>
2020-05-07 19:15:03 +0100

    variable string formatting

--- string-format.py
+++ string-format.py
@@ -4,3 +4,13 @@ like = 'rollerskating'
 age_description = 'My age is {} and I like {}.'.format(age, like)
 
 print age_description
+
+place = 'Mitte'
+
+location = 'Berlin'
+
+feeling = 'cool'
+
+where_do_you_live = 'I live in {0}, {1}. It\'s a {2} place to live!'.format(place, location, feeling) 
+
+print where_do_you_live
